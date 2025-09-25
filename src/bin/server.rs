04bf169dd37e898dbbdef874f9914355f98d51ef
Shubham Mishra <grapheo12@gmail.com>
2025-09-25 12:31:54 -0700

    app_banking feature flag

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -75,7 +75,13 @@ fn get_feature_set() -> (&'static str, &'static str) {
 
 enum NodeType {
     Sequencer(sequencer::SequencerNode),
+
+    #[cfg(feature = "app_banking")]
+    Worker(worker::PSLWorker<worker::engines::AbortableKVSTask>),
+
+    #[cfg(not(feature = "app_banking"))]
     Worker(worker::PSLWorker<worker::engines::KVSTask>),
+
     Storage(storage_server::StorageNode),
 
 }
@@ -87,7 +93,12 @@ async fn run_sequencer(cfg: Config) -> NodeType {
 
 
 async fn run_worker(cfg: PSLWorkerConfig) -> NodeType {
+    #[cfg(feature = "app_banking")]
+    let node = worker::PSLWorker::<worker::engines::AbortableKVSTask>::new(cfg);
+
+    #[cfg(not(feature = "app_banking"))]
     let node = worker::PSLWorker::<worker::engines::KVSTask>::new(cfg);
+
     NodeType::Worker(node)
 }
 
